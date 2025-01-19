ef98f207aaa9528e6b49efe087db8c9e3586cb3f
Volodymyr Vladykin <vova@pocketguard.com>
2025-01-19 20:22:30 +0200

    task 3 complete

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,16 @@
-function getElementWidth(content, padding, border) {
-  // assumes that we have border-box sizing enabled
-  return (
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2
-  );
+function filterArray(numbers, value) {
+  const result = [];
+
+  for (const number of numbers) {
+    if (number > value) result.push(number);
+  }
+
+  return result;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 console.log("----------------"); // to have nice padding between tasks output in console
